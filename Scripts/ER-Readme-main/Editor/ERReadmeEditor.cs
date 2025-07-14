c4dedaddf0d843af3b0fe7cf0e5cd920a00a3cfe
Eden Rompas <edenrompaskanter@gmail.com>
2025-07-14 14:45:12 +0800

    Refactor and reorganize ER-Readme scripts
    Moved ERReadme and its editor script to Scripts/ER-Readme-main, separating runtime and editor code. Added ERReadme.cs as a ScriptableObject for readme data, and renamed the editor script to ERReadmeEditor for clarity. Updated associated .meta files accordingly.

--- Scripts/ER-Readme-main/Editor/ERReadmeEditor.cs
+++ Scripts/ER-Readme-main/Editor/ERReadmeEditor.cs
@@ -1,19 +1,10 @@
+#if UNITY_EDITOR
+
 using UnityEditor;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "README", menuName = "Readme/New Readme", order = 1)]
-public class ERReadme : ScriptableObject
-{
-    [SerializeField] public string Title;
-
-    [TextArea(10, 40)]
-    [SerializeField] public string Description;
-}
-
-#if UNITY_EDITOR
-
 [CustomEditor(typeof(ERReadme))]
-public class ReadmeAssetEditor : Editor
+public class ERReadmeEditor : Editor
 {
     private bool _isEditMode = false;
     private Vector2 _scrollPos;
